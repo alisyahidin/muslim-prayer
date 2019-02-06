8ff17069c98b5bcd910f246dcb6ec8352137f353
Ali Syahidin <ali.idinz@gmail.com>
2019-02-06 14:52:35 +0700

    remove unuse cmp

--- src/components/PrayerScreen/Loader.js
+++ src/components/PrayerScreen/Loader.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import { timer } from 'rxjs'
 
 import PrayerScreenAnimation from '../../contexts/PrayerScreenAnimation'
 import Screen from './Screen'
